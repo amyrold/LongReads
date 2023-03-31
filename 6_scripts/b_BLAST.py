60e4ce7c7600856bc1cca508595f3ce4c6161214
amyrold <66313537+amyrold@users.noreply.github.com>
2023-03-31 16:17:57 -0500

    BLAST query
    Here is test code to BLAST our seqs against an E. coli 16S reference genome and create a csv that stores qstart and qend to allow us to sub-string our sequence.

--- 6_scripts/b_BLAST.py
+++ 6_scripts/b_BLAST.py
@@ -1,27 +1,59 @@
-Query = '"Betaherpesvirinae"[Organism] OR Betaherpesvirinae[All Fields]'
-Command = f'esearch -db Nucleotide -query {Query} | efetch -format fasta > BetaherpesvirinaeData.txt'
-#The command will use esearch to read the query. The -db will specfiy the datatbase. Query will connect the search with esearch.
-#efetch will also work with esearch. The format will then format the file into an output file, which will be BetaherpesvirinaeData.txt
 
-os.system(Command)
+import os
+import subprocess
+from Bio import Entrez
+from Bio import SeqIO
+# from Bio.Blast import NCBIWWW
 
-InFile = 'BetData.txt'
-OutFile = 'Betaherpesvirinae'
-database_type = 'nucl'
-title = 'Betaherpesvirinae'
+# determine the path of the directory this file is located in
+# idea taken from here: https://www.pythonanywhere.com/forums/topic/13464/
+my_env = os.path.join(os.path.dirname(__file__)) #comment out unless running as script
+# my_env = '/Users/aaronmyrold/GitHub/LongReads/6_scripts' #aaron
+# my_env = '' #niru
+# my_env = '' #japani
+# my_env = '' #asad
+# set the current working directory to that folder so that remaining paths can function properly
+os.chdir(my_env+ '/..')
 
-os.system(f'makeblastdb -in {input_file} -out {output_name} -title {title} -dbtype {database_type}')
-# In will be for the input file. Out will be for the output file. Title will be the name of the database while dbtyep of fpr the type that was used. 
 
-#Blasting
-#Uses longest contig
-inputFile = 'Longest.txt'
-outputFile = 'BetResults.csv'
-Blast = 'blastn -query ' + inputFile + ' -db Betaherpesvirinae -out ' + outputFile + ' -outfmt "6 sacc pident length qstart qend sstart send bitscore evalue stitles" > blastout.tsv '
-print(Blast)
 
-os.system(Blast)
+# PART 0 ----
+# Make directories and paths
+folder_names = ('1_raw_data', '2_filtered_data', '3_test_data', '4_output', '5_blast', '6_scripts')
+p_raw_data = folder_names[0]
+p_filt_data = folder_names[1]
+p_test_data = folder_names[2]
+p_out = folder_names[3]
+p_blast = folder_names[4]
+p_scripts = folder_names[5]
 
-#making table of blast command outputs
-os.system('echo "sacc pident length qstart qend sstart send bitscore evalue stitle" | cat - BetResults.csv > BetResults.tsv')
-os.system('head -n 11 BetResults.tsv  > BetResults2.tsv')
+# this is primarily due to github not saving empty directories
+# need to be able to create folder structure from scratch if needed  
+for i in folder_names:
+    if not os.path.exists(i):
+        os.makedirs(i)
+
+entrezQuery = "J01859"
+os.system(f'esearch -db nucleotide -query "{entrezQuery}" | efetch -format fasta > {p_blast}/16s.fasta')
+
+# Make blast database
+os.system(f'makeblastdb -in {p_blast}/blast.fasta -out {p_blast}/ec16S -title 16S -dbtype nucl')
+
+
+# Make blast query
+input_file = f'{p_raw_data}/wgs.fasta'
+output_file = f'{p_blast}/myresults.csv'
+# using the formatting requested
+formatting = '10 sacc pident qstart qend evalue'
+os.system(f'blastn -query {input_file} -db {p_blast}/BPvirus -out {output_file} -outfmt "{formatting}"')
+
+
+
+
+
+
+
+
+
+
+
