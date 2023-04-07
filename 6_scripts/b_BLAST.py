fe45c0ec64c8f68751fbb9b01f6732cc4c19c4bb
amyrold <66313537+amyrold@users.noreply.github.com>
2023-04-07 13:30:42 -0500

    Finalizing the BLAST script workflow
    This script will take the output of out data_download script and create a SeqIO dictionary with the trimmed 16S sequences.

--- 6_scripts/b_BLAST.py
+++ 6_scripts/b_BLAST.py
@@ -1,7 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 27 18:23:37 2023
+
+@author: aaronmyrold
+"""
+
 import os
-import subprocess
 import pandas as pd
-from Bio import Entrez
 from Bio import SeqIO
 # from Bio.Blast import NCBIWWW
 
@@ -33,26 +39,46 @@ for i in folder_names:
     if not os.path.exists(i):
         os.makedirs(i)
 
-entrezQuery = "J01859"
+# PART 1 ----
+# BLAST wgs sequences against 16S BLAST database
+# Create 16S BLAST database:
+entrezQuery = "J01859" #determine query
 os.system(f'esearch -db nucleotide -query "{entrezQuery}" | efetch -format fasta > {p_blast}/16s.fasta')
 
-# Make blast database
-os.system(f'makeblastdb -in {p_blast}/blast.fasta -out {p_blast}/16S -title 16S -dbtype nucl')
+# Use reference sequences to create BLAST database
+os.system(f'makeblastdb -in {p_blast}/16s.fasta -out {p_blast}/16S -title 16S -dbtype nucl')
 
-
-# Make blast query
+# Define BLASTn query
 input_file = f'{p_raw_data}/wgs.fasta'
 output_file = f'{p_blast}/myresults.csv'
 # using the formatting requested
-formatting = '10 sacc pident qstart qend evalue'
+formatting = '10 qacc pident qstart qend evalue'
+# Call the BLASTn query
 os.system(f'blastn -query {input_file} -db {p_blast}/16S -out {output_file} -outfmt "{formatting}"')
 
 
+# PART 2 -
+# Trim wgs reads to just the regions determined via 16S BLAST
+# Read in the BLASTn output as a pandas table
+pos16S = pd.read_csv(f'{output_file}', names=['accession','pident','start','end','ev']) 
+# read in the multi-fasta from data_download script
+wgs_dict = SeqIO.to_dict(SeqIO.parse(f'{p_raw_data}/wgs.fasta', 'fasta')) 
+# define dictionary to store trimmed sequences
+trim_dict = {}
 
-tsv = pd.read_csv(f'{p_raw_data}/{input_file}') #replace the path with where the tsv file is
+# Function to use the BLASTn output to trim and store in new dict 
+def trim_fa(accession, start, stop):
+    trim_dict[accession] = wgs_dict[accession][start:stop]
+    return
 
+# create function to send variables to trim_fa()
+def store_16S(row):
+    # take accession, start, stop and use to store the shortened sequence into new seq list
+    trim_fa(row['accession'],row['start'], row['end'])
+    return
 
-
+# apply the store_16S function across all rows in out BLAST output pd table
+pos16S.apply(store_16S, axis=1)
 
 
 
