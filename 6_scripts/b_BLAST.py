69c9d51421f4dc3a6cb0fb6b2c5a30fadea44d24
amyrold <66313537+amyrold@users.noreply.github.com>
2023-04-11 08:23:04 -0500

    pathing changes

--- 6_scripts/b_BLAST.py
+++ 6_scripts/b_BLAST.py
@@ -51,7 +51,7 @@ os.system(f'makeblastdb -in {p_blast}/16s.fasta -out {p_blast}/16S -title 16S -d
 input_file = f'{p_raw_data}/wgs.fasta'
 output_file = f'{p_blast}/myresults.csv'
 # using the formatting requested
-formatting = '10 qacc pident qstart qend evalue'
+formatting = '10 qacc pident qstart qend length evalue'
 # Call the BLASTn query
 os.system(f'blastn -query {input_file} -db {p_blast}/16S -out {output_file} -outfmt "{formatting}"')
 
@@ -77,14 +77,14 @@ os.system(f'blastn -query {input_file} -db {p_blast}/16S -out {output_file} -out
 # PART 2 ----
 # Trim wgs reads to just the regions determined via 16S BLAST
 # Read in the BLASTn output as a pandas table
-pos16S = pd.read_csv(f'{output_file}', names=['accession','pident','start','end','ev']) 
+pos16S = pd.read_csv(f'{output_file}', names=['accession','pident','start','end', 'length','ev']) 
 # read in the multi-fasta from data_download script
 wgs_dict = SeqIO.to_dict(SeqIO.parse(f'{p_raw_data}/wgs.fasta', 'fasta')) 
 # define dictionary to store trimmed sequences
 trim_dict = {}
 # Create trim.csv output file
-trim_temp = pd.DataFrame(columns=['accession','pident','start','end','ev'])
-trim_temp.to_csv('trim.csv', index=False)
+trim_temp = pd.DataFrame(columns=['accession','pident','start','end','length', 'ev'])
+trim_temp.to_csv(f'{p_filt_data}/trim.csv', index=False)
 
 # Define a function to find each unique accession and apply the rename_acc function to each accession
 def split_acc(seq_df):
@@ -103,14 +103,15 @@ def rename_acc(row):
     for i in range(1, len(subset['accession'])+1):
         subset['accession'][i-1] = f'{row[0]}_{i}'
     # append the data to our trim.csv without headers or index
-    subset.to_csv('trim.csv',mode='a',index=False,header=False)
+    subset.to_csv(f'{p_filt_data}/trim.csv',mode='a',index=False,header=False)
     return 
 
 # Call the split function on our myresults.csv dataframe (pos16S)
 split_acc(pos16S)
 
 # Read in the resulting dataframe with proper formatting
-trim_16S = pd.read_csv('trim.csv', dtype={'start':'Int32','end':'Int32'}) 
+trim_16S = pd.read_csv(f'{p_filt_data}/trim.csv', dtype={'start':'Int32','end':'Int32'}) 
+trim_16S_f = trim_16S.mask(trim_16S['length'] < 1400).dropna()
 
 # Function to use the BLASTn output to trim and store in new dict 
 def trim_fa(accession, start, stop):
@@ -125,10 +126,9 @@ def store_16S(row):
     return
 
 # apply the store_16S function across all rows in out BLAST output pd table
-trim_16S.apply(store_16S, axis=1)
-
+trim_16S_f.apply(store_16S, axis=1)
 
 # PART 3 ----
 # format and write to output
-with open(f"{p_filt_data}/trim2.fasta", "w") as output_handle:
+with open(f"{p_filt_data}/trim.fasta", "w") as output_handle:
     SeqIO.write(trim_dict.values(), output_handle, "fasta")
