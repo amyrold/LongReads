6ccf72143a6d0c8d7770f77e9284bda9b3164f7f
nshanbhag123 <ShanbhagN123@gmail.com>
2023-04-13 10:04:26 -0500

    added code to convert df and list to csv

--- 6_scripts/c_edit_distance.py
+++ 6_scripts/c_edit_distance.py
@@ -1,18 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr  7 13:50:49 2023
-
-@author: aaronmyrold
-"""
-
 import os
 import pandas as pd
 from Bio import SeqIO
 import itertools
 import numpy as np
 import editdistance
-# from Bio.Blast import NCBIWWW
 
 # determine the path of the directory this file is located in
 # idea taken from here: https://www.pythonanywhere.com/forums/topic/13464/
@@ -23,7 +14,6 @@ my_env = os.path.join(os.path.dirname(__file__)) #comment out unless running as 
 # my_env = '' #asad
 # set the current working directory to that folder so that remaining paths can function properly
 os.chdir(my_env+ '/..')
-
 
 
 # PART 0 ----
@@ -50,10 +40,11 @@ for key,value in records.items(): #append id to headers list
     headers.append(key)
 
 #initializing matrix
-df = pd.DataFrame(np.zeros((len(headers),len(headers))),
+ed_df = pd.DataFrame(np.zeros((len(headers),len(headers))),
              columns=headers,
              index=headers)
 
+#comb list will be populated with all the different pairings of accession nums
 comb_list = []
 for k in itertools.combinations(headers,2):
     comb_list.append(k)
@@ -63,59 +54,60 @@ def ed(seq_pair):
     seq1 = seq_pair[0]
     seq2 = seq_pair[1]
     
-    #print(editdistance.eval(seq1,seq2))
+    ed_df[seq1][seq2] = editdistance.eval(seq1,seq2)
     
-    df[seq1][seq2] = editdistance.eval(seq1,seq2)
-    
+#populates the within_coords and between_coords lists, depending on if the pairs are within the same genome or different genomes
 def within_between(seq_pair):
     seq1 = seq_pair[0]
     seq2 = seq_pair[1]
     
     if seq1[0:13] == seq2[0:13]:
-        within_coords.append(seq_pair)
+        within_coords.append(seq_pair) #appending the within
     else:
-        between_coords.append(seq_pair)
+        between_coords.append(seq_pair) #appending the between
         
+#get edit distance as iterating through the within coordinates
 def get_ed_within(seq_pair):
     seq1 = seq_pair[0]
     seq2 = seq_pair[1]
     
-    ed = df[seq1][seq2] 
+    ed = ed_df[seq1][seq2] 
     
-    within_ed.append(ed)
+    if seq_pair in within_coords:
+        within_ed.append(ed)
     
+#get edit distance as iterating through the between coordinates
 def get_ed_between(seq_pair):
     seq1 = seq_pair[0]
     seq2 = seq_pair[1]
     
-    ed = df[seq1][seq2] 
+    ed = ed_df[seq1][seq2] 
     
-    between_ed.append(ed)
+    if seq_pair in between_coords:
+        between_ed.append(ed)
     
-        
+#passing the ed function to the list of combinations list
 matrix_result = list(map(ed, comb_list))
 
+#initializing a list of within and between coordinates
 within_coords = []
 between_coords = []
+
+#passing the within_between function to the combinations list
 within_between_result = list(map(within_between, comb_list))
 
+#initializing a list of within and between edit distances. We will later do some stats on these
 within_ed = []
 between_ed = []
-get_ed_within_result = list(map(get_ed_within,within_coords))
+
+#passing the get_ed_within function to the list of within coordinates
+get_ed_within_result = list(map(get_ed_within,within_coords)) 
+
+#passing the get_ed_between function to the list of between coordinates
 get_ed_between_result = list(map(get_ed_between,between_coords))    
 
-
-
-#%%
-def ed(x, seq1, seq2):
-    index = x.index.values
-    seq1 = records[seq1].seq
-    seq2 = records[seq2].seq
-    
-    return(editdistance.eval(seq1,seq2))
-
-ed("NZ_CP007265.1_2", "NZ_CP007390.1_9")
-    
-
-    
-df.apply(lambda x: x.index.values)
+#sending the matrix, between_ed list, and within_ed list to csvs. 
+#these csvs will be used in R for hierarchical clustering
+ed_df.to_csv("/Users/nirushanbhag/Downloads/LongReads/matrix.csv")
+pd.Series(within_ed).to_csv("/Users/nirushanbhag/Downloads/LongReads/within.csv", header = None)
+pd.Series(between_ed).to_csv("/Users/nirushanbhag/Downloads/LongReads/between.csv", header = None)
